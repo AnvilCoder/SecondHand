6db2fa47ff18932a5e3167cdf42070ddcef65a39
Ldv236 <Ldv236@yandex.ru>
2023-12-10 23:26:42 +0200

    feature/8-users_dto_controller

--- src/main/java/ru/ac/secondhand/dto/user/UpdateUser.java
+++ src/main/java/ru/ac/secondhand/dto/user/UpdateUser.java
@@ -1,28 +1,23 @@
 package ru.ac.secondhand.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import ru.ac.secondhand.entity.enums.Role;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
-public class Register {
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateUser {
 
-    @Size(min = 4, max = 32)
-    private String username;
-
-    @Size(min = 8, max = 16)
-    private String password;
-
-    @Size(min = 2, max = 16)
+    @Size(min = 3, max = 10)
     private String firstName;
 
-    @Size(min = 2, max = 16)
+    @Size(min = 3, max = 10)
     private String lastName;
 
     @Pattern(regexp = "\\+7\\s?\\(?\\d{3}\\)?\\s?\\d{3}-?\\d{2}-?\\d{2}")
     private String phone;
-
-    private Role role;
 }
