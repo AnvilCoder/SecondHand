f9e931a610b1b8cd1104a2b67c7c8f9e7b3e49c9
Ldv236 <Ldv236@yandex.ru>
2023-12-15 23:26:59 +0200

    SCRUM-8 try add commit to jira ticket

--- src/main/java/ru/ac/secondhand/service/impl/UserServiceImpl.java
+++ src/main/java/ru/ac/secondhand/service/impl/UserServiceImpl.java
@@ -1,14 +1,19 @@
 package ru.ac.secondhand.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.ac.secondhand.dto.user.NewPassword;
 import ru.ac.secondhand.dto.user.UpdateUserDTO;
 import ru.ac.secondhand.dto.user.UserDTO;
+import ru.ac.secondhand.mapper.UserMapper;
 import ru.ac.secondhand.service.UserService;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserMapper userMapper;
 
     @Override
     public void setPassword(NewPassword newPassword) {
