48a30a274b68727a529b5d92e708a5af7e868af7
x3imal <aasurov@gmail.com>
2023-12-19 10:29:11 +0300

    27: add logic in CommentServiceImpl

--- src/main/java/ru/ac/secondhand/service/impl/CommentServiceImpl.java
+++ src/main/java/ru/ac/secondhand/service/impl/CommentServiceImpl.java
@@ -1,15 +1,33 @@
 package ru.ac.secondhand.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.ac.secondhand.dto.ad.ExtendedAd;
 import ru.ac.secondhand.dto.comment.CommentDTO;
 import ru.ac.secondhand.dto.comment.Comments;
 import ru.ac.secondhand.dto.comment.CreateOrUpdateComment;
+import ru.ac.secondhand.entity.Ad;
+import ru.ac.secondhand.repository.AdRepository;
+import ru.ac.secondhand.repository.CommentRepository;
+import ru.ac.secondhand.service.AdService;
 import ru.ac.secondhand.service.CommentService;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
 public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final AdService adService;
+
+
+
     @Override
     public Comments getComments(Integer adId) {
+        ExtendedAd ad = adService.getAdInfo(adId);
         return null;
     }
 
