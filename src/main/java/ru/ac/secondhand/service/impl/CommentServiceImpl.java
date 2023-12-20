cc29af26442de8f750dbf2c487160305225e603e
x3imal <aasurov@gmail.com>
2023-12-20 19:59:56 +0300

    27: add javaDocs in CommentServiceImpl class(createComment, getComments, delete,updateComment)

--- src/main/java/ru/ac/secondhand/service/impl/CommentServiceImpl.java
+++ src/main/java/ru/ac/secondhand/service/impl/CommentServiceImpl.java
@@ -29,7 +29,13 @@ public class CommentServiceImpl implements CommentService {
     private final AdRepository adRepository;
     private final CommentMapper mapper;
 
-
+    /**
+     * Получает объявление по его идентификатору.
+     *
+     * @param adId Идентификатор объявления, которое нужно получить.
+     * @return Найденное объявление.
+     * @throws AdNotFoundException если объявление с указанным идентификатором не найдено.
+     */
     private Ad getAdById(Integer adId) {
         log.info("Fetching Ad with id: {}", adId);
         return adRepository.findById(adId).orElseThrow(() -> {
@@ -38,6 +44,12 @@ public class CommentServiceImpl implements CommentService {
         });
     }
 
+    /**
+     * Получает все комментарии к конкретному объявлению.
+     *
+     * @param adId Идентификатор объявления, для которого нужно получить комментарии.
+     * @return Объект Comments, содержащий список комментариев и их количество.
+     */
     @Override
     @Transactional(readOnly = true)
     public Comments getComments(Integer adId) {
@@ -47,6 +59,13 @@ public class CommentServiceImpl implements CommentService {
         return mapper.toComments(comments);
     }
 
+    /**
+     * Создает новый комментарий к объявлению.
+     *
+     * @param comment Данные для создания нового комментария.
+     * @param adId Идентификатор объявления, к которому будет добавлен комментарий.
+     * @return Созданный комментарий в виде DTO.
+     */
     @Override
     public CommentDTO createComment(CreateOrUpdateComment comment, Integer adId) {
         log.info("Attempting to create a new comment for Ad [{}]", adId);
@@ -63,6 +82,13 @@ public class CommentServiceImpl implements CommentService {
         return mapper.toCommentDTO(saveComment);
     }
 
+    /**
+     * Удаляет комментарий из объявления.
+     *
+     * @param adId Идентификатор объявления, из которого будет удален комментарий.
+     * @param commentId Идентификатор удаляемого комментария.
+     * @throws CommentNotFoundException если комментарий с указанным идентификатором не найден.
+     */
     @Override
     public void delete(Integer adId, Integer commentId) {
         log.info("Trying to delete a comment for id: {}", adId);
@@ -76,12 +102,21 @@ public class CommentServiceImpl implements CommentService {
         log.info("Comments with id: {} successfully deleted for ad with id: {}", commentId, adId);
     }
 
+    /**
+     * Обновляет существующий комментарий к объявлению.
+     *
+     * @param adId Идентификатор объявления, к которому принадлежит комментарий.
+     * @param commentId Идентификатор комментария, который нужно обновить.
+     * @param commentRequest Обновленные данные комментария.
+     * @return Обновленный комментарий в виде DTO.
+     * @throws CommentNotFoundException если комментарий с указанным идентификатором не найден.
+     */
     @Override
     public CommentDTO updateComment(Integer adId, Integer commentId, CreateOrUpdateComment commentRequest) {
         log.info("Starting update of comment [{}] for ad [{}]", commentId, adId);
         Ad ad = getAdById(adId);
         Comment updateComment = commentRepository.findById(commentId)
-                .orElseThrow(()-> {
+                .orElseThrow(() -> {
                     log.warn("Comment [{}] not found for update", commentId);
                     return new CommentNotFoundException(String.format("Comment [%d] not found ", commentId));
                 });
