ebdfe7c4a0b4261306041254b4740b384d1d3a35
fifimova <fi_fimova@mail.ru>
2023-12-20 11:21:15 +0300

    - тестики

--- src/main/java/ru/ac/secondhand/dto/ad/AdDTO.java
+++ src/main/java/ru/ac/secondhand/dto/ad/AdDTO.java
@@ -1,8 +1,12 @@
 package ru.ac.secondhand.dto.ad;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdDTO { // response
 
     private Integer author;
--- src/main/java/ru/ac/secondhand/dto/ad/Ads.java
+++ src/main/java/ru/ac/secondhand/dto/ad/Ads.java
@@ -1,10 +1,14 @@
 package ru.ac.secondhand.dto.ad;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ads {
 
     private Integer count;
