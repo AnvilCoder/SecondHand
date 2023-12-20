6624b8c96b2f7ab2c45225e75fd16833cfaeccf2
x3imal <aasurov@gmail.com>
2023-12-20 19:56:02 +0300

    27: add logic in CommentServiceImpl class(createComment, getComments, delete,updateComment)

--- src/main/java/ru/ac/secondhand/repository/CommentRepository.java
+++ src/main/java/ru/ac/secondhand/repository/CommentRepository.java
@@ -3,5 +3,14 @@ package ru.ac.secondhand.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.ac.secondhand.entity.Comment;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    List<Comment> findByAdId(Integer adId);
+
+    Optional<Comment> findByAdIdAndId(Integer adId, Integer commentId);
+
+
 }
--- src/main/java/ru/ac/secondhand/service/impl/CommentServiceImpl.java
+++ src/main/java/ru/ac/secondhand/service/impl/CommentServiceImpl.java
@@ -4,15 +4,20 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.ac.secondhand.dto.ad.ExtendedAd;
 import ru.ac.secondhand.dto.comment.CommentDTO;
 import ru.ac.secondhand.dto.comment.Comments;
 import ru.ac.secondhand.dto.comment.CreateOrUpdateComment;
 import ru.ac.secondhand.entity.Ad;
+import ru.ac.secondhand.entity.Comment;
+import ru.ac.secondhand.exception.AdNotFoundException;
+import ru.ac.secondhand.exception.CommentNotFoundException;
+import ru.ac.secondhand.mapper.CommentMapper;
 import ru.ac.secondhand.repository.AdRepository;
 import ru.ac.secondhand.repository.CommentRepository;
-import ru.ac.secondhand.service.AdService;
 import ru.ac.secondhand.service.CommentService;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,28 +26,72 @@ import ru.ac.secondhand.service.CommentService;
 public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
-    private final AdService adService;
+    private final AdRepository adRepository;
+    private final CommentMapper mapper;
 
 
-
-    @Override
-    public Comments getComments(Integer adId) {
-        ExtendedAd ad = adService.getAdInfo(adId);
-        return null;
+    private Ad getAdById(Integer adId) {
+        log.info("Fetching Ad with id: {}", adId);
+        return adRepository.findById(adId).orElseThrow(() -> {
+            log.warn("Ad not found for id: {}", adId);
+            return new AdNotFoundException("Ad not found for id: " + adId);
+        });
     }
 
     @Override
-    public CommentDTO createComment(CreateOrUpdateComment comment) {
-        return null;
+    @Transactional(readOnly = true)
+    public Comments getComments(Integer adId) {
+        log.info("Fetching comments for adId: {}", adId);
+        Ad ad = getAdById(adId);
+        List<Comment> comments = commentRepository.findByAdId(adId);
+        return mapper.toComments(comments);
+    }
+
+    @Override
+    public CommentDTO createComment(CreateOrUpdateComment comment, Integer adId) {
+        log.info("Attempting to create a new comment for Ad [{}]", adId);
+        Ad ad = getAdById(adId);
+
+        Comment newComment = new Comment();
+        newComment.setText(comment.getText());
+        newComment.setAd(ad);
+        newComment.setUser(ad.getUser());
+        newComment.setCreatedAt(LocalDateTime.now());
+
+        Comment saveComment = commentRepository.save(newComment);
+        log.info("Comment [{}] successfully created for Ad [{}]", saveComment.getId(), adId);
+        return mapper.toCommentDTO(saveComment);
     }
 
     @Override
     public void delete(Integer adId, Integer commentId) {
-
+        log.info("Trying to delete a comment for id: {}", adId);
+        getAdById(adId);
+        Comment deleteComment = commentRepository.findByAdIdAndId(adId, commentId)
+                .orElseThrow(() -> {
+                    log.warn("Comment [{}] not found for update", commentId);
+                    return new CommentNotFoundException(String.format("Comment [%d] not found for ad [%d]", commentId, adId));
+                });
+        commentRepository.delete(deleteComment);
+        log.info("Comments with id: {} successfully deleted for ad with id: {}", commentId, adId);
     }
 
     @Override
     public CommentDTO updateComment(Integer adId, Integer commentId, CreateOrUpdateComment commentRequest) {
-        return null;
+        log.info("Starting update of comment [{}] for ad [{}]", commentId, adId);
+        Ad ad = getAdById(adId);
+        Comment updateComment = commentRepository.findById(commentId)
+                .orElseThrow(()-> {
+                    log.warn("Comment [{}] not found for update", commentId);
+                    return new CommentNotFoundException(String.format("Comment [%d] not found ", commentId));
+                });
+        updateComment.setText(commentRequest.getText());
+        updateComment.setAd(ad);
+        updateComment.setUser(ad.getUser());
+        updateComment.setCreatedAt(LocalDateTime.now());
+        commentRepository.save(updateComment);
+        log.info("Comment [{}] for ad [{}] successfully updated", commentId, adId);
+
+        return mapper.toCommentDTO(updateComment);
     }
 }
