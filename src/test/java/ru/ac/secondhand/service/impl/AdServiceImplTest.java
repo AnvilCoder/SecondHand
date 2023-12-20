d12038abb245fdc01b230b0022b4d3f3f724d14e
x3imal <aasurov@gmail.com>
2023-12-20 22:16:38 +0300

    28: add one test for AdServiceImpl(getAdById)

--- src/test/java/ru/ac/secondhand/service/impl/AdServiceImplTest.java
+++ src/test/java/ru/ac/secondhand/service/impl/AdServiceImplTest.java
@@ -15,6 +15,7 @@ import ru.ac.secondhand.dto.ad.ExtendedAd;
 import ru.ac.secondhand.entity.Ad;
 import ru.ac.secondhand.entity.Image;
 import ru.ac.secondhand.entity.User;
+import ru.ac.secondhand.exception.AdNotFoundException;
 import ru.ac.secondhand.mapper.AdMapper;
 import ru.ac.secondhand.repository.AdRepository;
 import ru.ac.secondhand.service.ImageService;
@@ -25,6 +26,11 @@ import javax.persistence.EntityNotFoundException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AdServiceImplTest {
@@ -45,13 +51,13 @@ class AdServiceImplTest {
     void shouldReturnAds() {
         List<Ad> ads = List.of(TestUtils.getAdEntity());
 
-        Mockito.when(adRepository.findAll()).thenReturn(ads);
-        Mockito.when(mapper.toAds(ads)).thenReturn(TestUtils.getAds());
+        when(adRepository.findAll()).thenReturn(ads);
+        when(mapper.toAds(ads)).thenReturn(TestUtils.getAds());
 
         Ads adsDTO = adService.getAll();
         Assertions.assertThat(adsDTO.getCount()).isEqualTo(ads.size());
         Assertions.assertThat(adsDTO.getResults().size()).isEqualTo(ads.size());
-        Mockito.verify(adRepository).findAll();
+        verify(adRepository).findAll();
     }
 
     @Test
@@ -59,11 +65,11 @@ class AdServiceImplTest {
         List<Ad> ads = Collections.emptyList();
         Ads adsExp = new Ads(0, Collections.emptyList());
 
-        Mockito.when(adRepository.findAll()).thenReturn(ads);
-        Mockito.when(mapper.toAds(ads)).thenReturn(adsExp);
+        when(adRepository.findAll()).thenReturn(ads);
+        when(mapper.toAds(ads)).thenReturn(adsExp);
 
         Assertions.assertThatNoException().isThrownBy(() -> adService.getAll());
-        Mockito.verify(adRepository).findAll();
+        verify(adRepository).findAll();
     }
 
     @Test
@@ -72,21 +78,21 @@ class AdServiceImplTest {
         Integer adId = ad.getId();
         ExtendedAd adDTO = TestUtils.getExtendedAd();
 
-        Mockito.when(adRepository.findById(adId)).thenReturn(Optional.of(ad));
-        Mockito.when(mapper.toExtendedAd(ad)).thenReturn(adDTO);
+        when(adRepository.findById(adId)).thenReturn(Optional.of(ad));
+        when(mapper.toExtendedAd(ad)).thenReturn(adDTO);
 
         ExtendedAd result = adService.getAdInfo(adId);
         Assertions.assertThat(result).isNotNull();
         Assertions.assertThat(result).hasNoNullFieldsOrProperties();
 
-        Mockito.verify(adRepository).findById(adId);
-        Mockito.verify(mapper).toExtendedAd(ad);
+        verify(adRepository).findById(adId);
+        verify(mapper).toExtendedAd(ad);
     }
 
     @Test
     void getAdInfoShouldThrowException() {
         Integer adId = TestUtils.AD_ID;
-        Mockito.when(adRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
+        when(adRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
 
         Assertions.assertThatThrownBy(
                         () -> adService.getAdInfo(adId))
@@ -100,17 +106,17 @@ class AdServiceImplTest {
         User user = TestUtils.getUserEntity();
         Integer userId = user.getId();
 
-        Mockito.when(userService.findUser()).thenReturn(user);
-        Mockito.when(adRepository.findAdsByUserId(userId)).thenReturn(ads);
-        Mockito.when(mapper.toAds(ads)).thenReturn(TestUtils.getAds());
+        when(userService.findUser()).thenReturn(user);
+        when(adRepository.findAdsByUserId(userId)).thenReturn(ads);
+        when(mapper.toAds(ads)).thenReturn(TestUtils.getAds());
 
         Ads result = adService.getUsersAds();
         Assertions.assertThat(result.getCount()).isEqualTo(ads.size());
         Assertions.assertThat(result.getResults().size()).isEqualTo(ads.size());
 
-        Mockito.verify(userService).findUser();
-        Mockito.verify(adRepository).findAdsByUserId(userId);
-        Mockito.verify(mapper).toAds(ads);
+        verify(userService).findUser();
+        verify(adRepository).findAdsByUserId(userId);
+        verify(mapper).toAds(ads);
     }
 
     @Test
@@ -120,12 +126,12 @@ class AdServiceImplTest {
         User user = TestUtils.getUserEntity();
         Integer userId = user.getId();
 
-        Mockito.when(userService.findUser()).thenReturn(user);
-        Mockito.when(adRepository.findAdsByUserId(userId)).thenReturn(ads);
-        Mockito.when(mapper.toAds(ads)).thenReturn(adsExp);
+        when(userService.findUser()).thenReturn(user);
+        when(adRepository.findAdsByUserId(userId)).thenReturn(ads);
+        when(mapper.toAds(ads)).thenReturn(adsExp);
 
         Assertions.assertThatNoException().isThrownBy(() -> adService.getUsersAds());
-        Mockito.verify(adRepository).findAdsByUserId(userId);
+        verify(adRepository).findAdsByUserId(userId);
     }
 
     @Test
@@ -135,14 +141,14 @@ class AdServiceImplTest {
         User user = TestUtils.getUserEntity();
         Integer userId = user.getId();
 
-        Mockito.when(userService.findUser()).thenReturn(user);
-        Mockito.when(mapper.toAdEntity(createOrUpdateAd)).thenReturn(ad);
-        Mockito.when(mapper.toAdDTO(ad)).thenReturn(TestUtils.getAdDTO());
+        when(userService.findUser()).thenReturn(user);
+        when(mapper.toAdEntity(createOrUpdateAd)).thenReturn(ad);
+        when(mapper.toAdDTO(ad)).thenReturn(TestUtils.getAdDTO());
 
         AdDTO result = adService.createAd(createOrUpdateAd);
         Assertions.assertThat(result).hasNoNullFieldsOrProperties();
         Assertions.assertThat(result.getAuthor()).isEqualTo(userId);
-        Mockito.verify(adRepository).save(ad);
+        verify(adRepository).save(ad);
     }
 
     @Test
@@ -151,20 +157,20 @@ class AdServiceImplTest {
         Ad ad = TestUtils.getAdEntity();
         Integer adId = ad.getId();
 
-        Mockito.when(adRepository.findById(adId)).thenReturn(Optional.of(ad));
-        Mockito.when(mapper.toAdDTO(ad)).thenReturn(TestUtils.getAdDTO());
+        when(adRepository.findById(adId)).thenReturn(Optional.of(ad));
+        when(mapper.toAdDTO(ad)).thenReturn(TestUtils.getAdDTO());
 
         AdDTO result = adService.updateAd(adId, createOrUpdateAd);
         Assertions.assertThat(result.getTitle()).isEqualTo(createOrUpdateAd.getTitle());
         Assertions.assertThat(result.getPrice()).isEqualTo(createOrUpdateAd.getPrice());
         Assertions.assertThat(result).hasNoNullFieldsOrProperties();
-        Mockito.verify(adRepository).save(ad);
+        verify(adRepository).save(ad);
     }
 
     @Test
     void updateAdShouldThrowException() {
         Integer adId = TestUtils.AD_ID;
-        Mockito.when(adRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
+        when(adRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
 
         Assertions.assertThatThrownBy(
                         () -> adService.updateAd(adId, TestUtils.getCreateOrUpdateAd()))
@@ -179,8 +185,8 @@ class AdServiceImplTest {
         MultipartFile image = TestUtils.getMultipartFile();
         Image newImage = TestUtils.getImage();
 
-        Mockito.when(adRepository.findById(adId)).thenReturn(Optional.of(adWithImage));
-        Mockito.when(imageService.saveImage(image)).thenReturn(newImage);
+        when(adRepository.findById(adId)).thenReturn(Optional.of(adWithImage));
+        when(imageService.saveImage(image)).thenReturn(newImage);
         if (adWithImage.getImage() != null) {
             Mockito.doNothing().when(imageService).deleteImage(adWithImage.getImage().getId());
         }
@@ -188,10 +194,10 @@ class AdServiceImplTest {
         String result = adService.updateAdImage(adId, image);
 
         Assertions.assertThat(result).isEqualTo("/ads/image/" + newImage.getId());
-        Mockito.verify(adRepository).save(adWithImage);
-        Mockito.verify(imageService).saveImage(image);
+        verify(adRepository).save(adWithImage);
+        verify(imageService).saveImage(image);
         if (adWithImage.getImage() != null) {
-            Mockito.verify(imageService).deleteImage(adWithImage.getImage().getId());
+            verify(imageService).deleteImage(adWithImage.getImage().getId());
         }
     }
 
@@ -200,7 +206,7 @@ class AdServiceImplTest {
         Integer adId = TestUtils.AD_ID;
         MultipartFile image = TestUtils.getMultipartFile();
 
-        Mockito.when(adRepository.findById(adId)).thenReturn(Optional.empty());
+        when(adRepository.findById(adId)).thenReturn(Optional.empty());
 
         Assertions.assertThatThrownBy(
                         () -> adService.updateAdImage(adId, image))
@@ -215,8 +221,8 @@ class AdServiceImplTest {
         Integer adId = adWithImage.getId();
         MultipartFile image = TestUtils.getMultipartFile();
 
-        Mockito.when(adRepository.findById(adId)).thenReturn(Optional.of(adWithImage));
-        Mockito.when(imageService.saveImage(image)).thenThrow(new RuntimeException("Image save failed"));
+        when(adRepository.findById(adId)).thenReturn(Optional.of(adWithImage));
+        when(imageService.saveImage(image)).thenThrow(new RuntimeException("Image save failed"));
 
         Assertions.assertThatThrownBy(() -> adService.updateAdImage(adId, image))
                 .isInstanceOf(RuntimeException.class)
@@ -228,10 +234,10 @@ class AdServiceImplTest {
         Ad ad = TestUtils.getAdEntity();
         Integer adId = ad.getId();
 
-        Mockito.when(adRepository.findById(adId)).thenReturn(Optional.of(ad));
+        when(adRepository.findById(adId)).thenReturn(Optional.of(ad));
 
         Assertions.assertThatNoException().isThrownBy(() -> adService.deleteAd(adId));
-        Mockito.verify(adRepository).delete(ad);
+        verify(adRepository).delete(ad);
     }
 
     @Test
@@ -239,10 +245,31 @@ class AdServiceImplTest {
         Ad ad = TestUtils.getAdEntity();
         Integer adId = ad.getId();
 
-        Mockito.when(adRepository.findById(adId)).thenReturn(Optional.empty());
+        when(adRepository.findById(adId)).thenReturn(Optional.empty());
 
         Assertions.assertThatThrownBy(() -> adService.deleteAd(adId))
                 .isInstanceOf(EntityNotFoundException.class)
                 .hasMessageContaining(String.format("Ad with [id=%s] not found", adId));
+    }
+
+    @Test
+    void getAdById_Success() {
+        Integer adId = TestUtils.AD_ID;
+        Ad expectedAd = TestUtils.getAdEntity();
+        when(adRepository.findById(adId)).thenReturn(Optional.of(expectedAd));
+
+        Ad actualAd = adService.getAdById(adId);
+
+        assertEquals(expectedAd, actualAd);
+        verify(adRepository).findById(adId);
+    }
+
+    @Test
+    void getAdById_NotFound_ThrowsAdNotFoundException() {
+        Integer adId = TestUtils.AD_ID;
+        when(adRepository.findById(adId)).thenReturn(Optional.empty());
+
+        assertThrows(AdNotFoundException.class, () -> adService.getAdById(adId));
+        verify(adRepository).findById(adId);
     }
 }
