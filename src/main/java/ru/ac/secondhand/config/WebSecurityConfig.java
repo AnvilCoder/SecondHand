5df72b4d5e94959f048e6cde7ce8c81e68e6f9e1
x3imal <aasurov@gmail.com>
2023-12-25 22:07:12 +0300

    48: еще доки

--- src/main/java/ru/ac/secondhand/config/WebSecurityConfig.java
+++ src/main/java/ru/ac/secondhand/config/WebSecurityConfig.java
@@ -11,6 +11,23 @@ import org.springframework.web.cors.CorsConfiguration;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+/**
+ * Конфигурация безопасности веб-приложения.
+ * <p>
+ * Этот класс аннотирован как {@code @EnableWebSecurity}, что позволяет настроить
+ * параметры безопасности приложения. Он определяет правила доступа для различных
+ * URL-адресов и конфигурирует кросс-доменные запросы (CORS) для разрешенных источников.
+ * </p>
+ * <p>
+ * Внутри этого класса определено белое списком URL-адресов ({@code AUTH_WHITELIST}),
+ * которые доступны без аутентификации. Также определены правила доступа для остальных
+ * URL-адресов, включая разрешенные HTTP-методы и заголовки.
+ * </p>
+ * <p>
+ * Класс также создает бин {@code PasswordEncoder} для хеширования паролей пользователей
+ * с использованием алгоритма BCrypt.
+ * </p>
+ */
 @EnableWebSecurity
 public class WebSecurityConfig {
 
