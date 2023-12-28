21b59ea86ecb426d3e7cc9d78b6824fe9f7c06b4
x3imal <aasurov@gmail.com>
2023-12-28 11:17:54 +0300

    48: вернул ImageController

---
+++ src/main/java/ru/ac/secondhand/controller/ImageController.java
@@ -0,0 +1,51 @@
+package ru.ac.secondhand.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import ru.ac.secondhand.entity.Image;
+import ru.ac.secondhand.service.ImageService;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/image")
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addImage(@RequestParam("image") MultipartFile image) {
+        Image imageUrl = imageService.saveImage(image);
+        if (imageUrl != null) {
+            return ResponseEntity.ok("Image added successfully: " + imageUrl);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to add image");
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteImage(@PathVariable Integer id) {
+        imageService.deleteImage(id);
+    }
+
+    @GetMapping(value = "/{imageId}", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_GIF_VALUE, "image/*"})
+    public ResponseEntity<?> getImageById(@PathVariable Integer imageId) {
+        byte[] image = imageService.getImage(imageId);
+        if (image != null) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+            return new ResponseEntity<>(image, headers, HttpStatus.OK);
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
